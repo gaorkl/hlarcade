43b70f5b9f77bc9c1816592784d63c8ba229dbf1
mgarciaortiz <sbrn692@pfsnsq860248.city.ac.uk>
2022-03-17 13:59:17 +0000

    Creating Frame Buffer

--- spg_test.py
+++ spg_test.py
@@ -7,16 +7,27 @@ from array import array
 # ctx = HLContext()
 # buffer = ctx.buffer(data=array('f', (0,1,2,3)))
 
-
 from gl_custom.context import Context
 ctx = Context()
 buffer = ctx.buffer(data=array('f', (0,1,4,3)))
 
 print(np.frombuffer(buffer.read(), dtype=np.float32))
 
-# from arcade.sprite import Sprite
-# image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
-# player_sprite = Sprite(image_source)
-# player_sprite.set_position(10, 10)
+fbo = ctx.framebuffer(
+    color_attachments=[
+        ctx.texture( (100, 100), components=4 )
+    ]
+)
+
+with fbo.activate() as fbo:
+    fbo.clear( (0,123,124) )
+
+print(np.frombuffer(fbo.read(), dtype=np.uint8).reshape(100, 100, 3))
+
+
+from arcade.sprite import Sprite
+image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+player_sprite = Sprite(image_source)
+player_sprite.set_position(10, 10)
 
 
